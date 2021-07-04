8c8974fbf3b9e51a9eeb6143858efde8669bb11b
sirbuandrei <andreisirbu03@yahoo.com>
2021-07-05 00:52:42 +0300

    renamed file

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
-import 'resuable_card.dart';
+import 'reusable_card.dart';
 import 'constants.dart';
 
 enum Gender {
